ad3683c6e16d6da5d69d96db5ef67fb2a8bd9fed
zhaodafei111 <1097625354@qq.com>
2022-08-22 14:58:00 +0800

    init_Vue3

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # temp_uniapp
-uni-app项目,一套代码集合了h5,微信小程序,Android版,iOS版等跨平台效果
+uni-app项目,一套代码集合了h5,微信小程序,Android版,iOS版等跨平台效果,支持Vue3
+
+### 环境说明
+
+```html
+vue-cli 版本 @vue/cli 5.0.8
+vue 3.2.37
+node 14.18.0
+开发工具 webstorm
+测试工具: chrome, 微信开发者工具
+```
+
+
 
 ### 启动
 
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,52 +1,52 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
+  <view class="content">
+    <image class="logo" src="/static/logo.png"></image>
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+
     <view>
       <text class="title"> 启动遇到报错,就是你的版本有问题,或者少依赖 </text>
     </view>
-	</view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: '大飞,uni-app启动成功了'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+  data() {
+    return {
+      title: '大飞Vue3,uni-app启动成功了',
+    }
+  },
+  onLoad() {},
+  methods: {},
+}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+.text-area {
+  display: flex;
+  justify-content: center;
+}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
 </style>
